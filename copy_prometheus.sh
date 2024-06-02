5d48b56912c9a27f770db21b9f5913beea594282
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-06-02 12:28:12 -0300

    Create copy_prometheus.sh

---
+++ copy_prometheus.sh
@@ -0,0 +1,33 @@
+
+#!/bin/bash
+# Função para analisar o código de retorno de um comando
+check_return_code() {
+    local return_code=$?
+    local message=$1
+
+    if [ $return_code -ne 0 ]; then
+        echo "Erro: $message (Código de retorno: $return_code)"
+        exit $return_code
+    else
+        echo "Sucesso: $message"
+    fi
+}
+
+# Verifica se o argumento foi passado
+if [ $# -eq 0 ]; then
+    echo "Uso: $0 nome_do_arquivo"
+    exit 1
+fi
+
+# Nome do arquivo passado como argumento
+FILE=$1
+
+# Verifica se o arquivo existe
+if [ -e "$FILE" ]; then
+    echo "O arquivo '$FILE' existe."
+    cp -Rf $FILE /etc/prometheus/
+    
+else
+    echo "O arquivo '$FILE' não existe."
+    exit 0
+fi
