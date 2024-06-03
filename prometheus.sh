2b98c5eded69705ae99ecd06dcb5f0e6e169ac98
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-06-03 08:10:18 -0300

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Defina a variável a ser validada
+IP_ADDRESS=$1
+
 # Função para analisar o código de retorno de um comando
 check_return_code() {
     local return_code=$?
@@ -11,7 +14,27 @@ check_return_code() {
         echo "Sucesso: $message"
     fi
 }
-echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando pacotes"
+
+# Verifica se a variável não está vazia
+if [ -z "$IP_ADDRESS" ]; then
+    echo "A variável está vazia. Por favor, forneça um endereço IP."
+    exit 1
+fi
+
+# Expressão regular para validar um endereço IPv4
+regex="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
+
+if [[ $IP_ADDRESS =~ $regex ]]; then
+    # Se a expressão regular corresponde, verificamos cada parte do IP
+    IFS='.' read -r -a octets <<< "$IP_ADDRESS"
+    for octet in "${octets[@]}"; do
+        if ((octet < 0 || octet > 255)); then
+            echo "Endereço IP inválido: cada octeto deve estar entre 0 e 255."
+            exit 1
+        fi
+    done
+    echo "Endereço IP válido."
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalando pacotes"
 dnf -y install zlib-devel pam-devel openssl-devel libtool bison flex autoconf gcc make git net-tools lsof net-tools
 check_return_code "Instalando pacotes"
 
@@ -38,3 +61,11 @@ check_return_code "Start Prometheus"
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Status Prometheus"
 systemctl status prometheus 
 check_return_code "Status Prometheus"
+else
+    echo "Endereço IP inválido: formato incorreto."
+    exit 1
+fi
+
+
+
+
