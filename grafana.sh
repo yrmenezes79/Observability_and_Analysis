46ddc563348de1bef0c5db6c2cb1e9bbcdf577f5
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-06-03 11:08:34 -0300

    Create grafana.sh

---
+++ grafana.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Função para analisar o código de retorno de um comando
+check_return_code() {
+    local return_code=$?
+    local message=$1
+
+    if [ $return_code -ne 0 ]; then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') Erro: $message (Código de retorno: $return_code)"
+        exit $return_code
+    else
+        echo "$(date '+%Y-%m-%d %H:%M:%S') Sucesso: $message"
+    fi
+}
+
+# Verifica se a variável não está vazia
+if [ -z "$IP_ADDRESS" ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') A variável está vazia. Por favor, forneça um endereço IP."
+    exit 1
+fi
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Adicionando repositorio"
+cp -Rf grafana.repo /etc/yum.repos.d/
+check_return_code "Adicionando repositorio"
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Update Policie"
+update-crypto-policies --set DEFAULT:SHA1 
+check_return_code "Update Policie"
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Instalação Grafana"
+dnf install grafana -y
+check_return_code "Instalação Grafana"
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Start Grafana"
+systemctl daemon-reload
+systemctl enable --now grafana-server
+check_return_code "Start Grafana"
+
+echo "$(date '+%Y-%m-%d %H:%M:%S') - Status Grafana"
+if systemctl is-active --quiet grafana; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Grafana está ativo."
+else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Grafana não está ativo."
+    exit 1
+fi
