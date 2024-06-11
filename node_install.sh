1a77d1e2689fc0adbeca30532970525b24c6b222
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-06-11 12:05:37 -0300

    Update node_install.sh

--- node_install.sh
+++ node_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Defina a variável a ser validada
-IP_ADDRESS=$1
+
 
 # Função para analisar o código de retorno de um comando
 check_return_code() {
@@ -14,26 +13,6 @@ check_return_code() {
         echo "$(date '+%Y-%m-%d %H:%M:%S') Sucesso: $message"
     fi
 }
-
-# Verifica se a variável não está vazia
-if [ -z "$IP_ADDRESS" ]; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') A variável está vazia. Por favor, forneça um endereço IP."
-    exit 1
-fi
-
-# Expressão regular para validar um endereço IPv4
-regex="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
-
-if [[ $IP_ADDRESS =~ $regex ]]; then
-    # Se a expressão regular corresponde, verificamos cada parte do IP
-    IFS='.' read -r -a octets <<< "$IP_ADDRESS"
-    for octet in "${octets[@]}"; do
-        if ((octet < 0 || octet > 255)); then
-            echo "$(date '+%Y-%m-%d %H:%M:%S') Endereço IP inválido: cada octeto deve estar entre 0 e 255."
-            exit 1
-        fi
-    done
-    echo "Endereço IP válido."
 
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Adicionando repositorio"
 cp -Rf prometheus-rpm_release.repo /etc/yum.repos.d/
